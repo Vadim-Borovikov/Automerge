39841a9a5599080d2292161d259d02ef98f62036
Netriss <vadim.borovikov@gmail.com>
2015-11-05 12:04:47 +0300

    Conflict + Test

---
+++ Automerger/Model/Conflict.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automerger.Model
+{
+    public class Conflict
+    {
+        public readonly int Start;
+        public readonly int RemovedAmount;
+        public readonly string[] NewContent1;
+        public readonly string[] NewContent2;
+
+        public Conflict(int start, int removedAmount,
+                        string[] newContent1, string[] newContent2)
+        {
+            if ((start < 0) || (removedAmount < 1))
+            {
+                throw new ArgumentException();
+            }
+            if ((newContent1 == null) || (newContent2 == null))
+            {
+                throw new ArgumentNullException();
+            }
+            if ((newContent1.Length == 0) || (newContent2.Length == 0))
+            {
+                throw new ArgumentException();
+            }
+
+            Start = start;
+            RemovedAmount = removedAmount;
+            NewContent1 = newContent1;
+            NewContent2 = newContent2;
+        }
+    }
+}
---
+++ AutomergerTests/Model/ConflictTests.cs
@@ -0,0 +1,30 @@
+﻿using AutomergerTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Automerger.Model.Tests
+{
+    [TestClass()]
+    public class ConflictTests
+    {
+        [TestMethod()]
+        public void ConflictTest()
+        {
+            string[] content1 = new string[1] { "Test1" };
+            string[] content2 = new string[1] { "Test2" };
+
+            MyAssert.Throws<ArgumentException>(() => new Conflict(-1, 1, content1, content2));
+            MyAssert.Throws<ArgumentException>(() => new Conflict(0, 0, content1, content2));
+
+            MyAssert.Throws<ArgumentNullException>(() => new Conflict(0, 1, null, content2));
+            MyAssert.Throws<ArgumentNullException>(() => new Conflict(0, 1, content1, null));
+
+            var conflict = new Conflict(0, 1, content1, content2);
+
+            Assert.IsTrue(conflict.Start == 0);
+            Assert.IsTrue(conflict.RemovedAmount == 1);
+            CollectionAssert.AreEqual(content1, conflict.NewContent1);
+            CollectionAssert.AreEqual(content2, conflict.NewContent2);
+        }
+    }
+}
