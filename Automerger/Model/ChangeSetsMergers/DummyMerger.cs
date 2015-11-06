f4753f2b53b1344e02b8e5ae47a23b64fc6acc1e
Vadim Borovikov <vadim.borovikov@gmail.com>
2015-11-06 13:21:24 +0300

    Tests for DummyMerger

--- Automerger/Model/ChangeSetsMergers/DummyMerger.cs
+++ Automerger/Model/ChangeSetsMergers/DummyMerger.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Automerger.Model
 {
+    /// <summary>
+    /// Merger that takes all from changes1 and nothing from changes2
+    /// </summary>
     public class DummyMerger : IChangeSetMerger
     {
         public IDictionary<int, IChange> Merge(IReadOnlyDictionary<int, IMergableChange> changes1,
                                                IReadOnlyDictionary<int, IMergableChange> changes2)
         {
+            if ((changes1 == null) || (changes2 == null))
+            {
+                throw new ArgumentNullException();
+            }
+
             var result = new Dictionary<int, IChange>();
 
-            foreach (KeyValuePair<int, IMergableChange> pair in changes1)
+            foreach (int key in changes1.Keys)
             {
-                result.Add(pair.Key, pair.Value);
+                result.Add(key, changes1[key]);
             }
 
             return result;
---
+++ AutomergerTests/Model/ChangeSetsMergers/DummyMergerTests.cs
@@ -0,0 +1,46 @@
+﻿using AutomergerTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Automerger.Model.Tests
+{
+    [TestClass()]
+    public class DummyMergerTests
+    {
+        [TestMethod()]
+        public void MergeTest()
+        {
+            var merger = new DummyMerger();
+
+            var changes1 = new Dictionary<int, IMergableChange>();
+            changes1.Add(1, new Addition(1, new string[] { "10" }));
+            changes1.Add(2, new Removal(2, 1));
+            changes1.Add(3, new Replacement(3, 2, new string[] { "20" }));
+
+            var changes2 = new Dictionary<int, IMergableChange>();
+            changes2.Add(1, new Addition(1, new string[] { "30" }));
+            changes2.Add(3, new Replacement(3, 2, new string[] { "40" }));
+
+            MyAssert.Throws<ArgumentNullException>(() => merger.Merge(null, changes2));
+            MyAssert.Throws<ArgumentNullException>(() => merger.Merge(changes1, null));
+
+            IDictionary<int, IChange> result = merger.Merge(changes1, changes2);
+            CheckDictionaries(changes1, result);
+
+            result = merger.Merge(changes2, changes1);
+            CheckDictionaries(changes2, result);
+        }
+
+        private void CheckDictionaries(Dictionary<int, IMergableChange> expected,
+                                       IDictionary<int, IChange> actual)
+        {
+            Assert.IsTrue(actual.Keys.SequenceEqual(actual.Keys));
+            foreach (int key in actual.Keys)
+            {
+                Assert.AreEqual(actual[key], expected[key]);
+            }
+        }
+    }
+}
