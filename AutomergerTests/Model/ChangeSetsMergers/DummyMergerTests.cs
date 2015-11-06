53bfbec8fa56d41025f185585a90c67ec6b37995
Vadim Borovikov <vadim.borovikov@gmail.com>
2015-11-06 18:47:58 +0300

    Collection initializers for DummyMergerTests

--- AutomergerTests/Model/ChangeSetsMergers/DummyMergerTests.cs
+++ AutomergerTests/Model/ChangeSetsMergers/DummyMergerTests.cs
@@ -12,14 +12,18 @@ namespace Automerger.Model.Tests
             var merger = new DummyMerger();
             MergersCommonTests.CommonTest(merger);
 
-            var changes1 = new Dictionary<int, IMergableChange>();
-            changes1[1] = new Addition(1, new string[] { "10" });
-            changes1[2] = new Removal(2, 1);
-            changes1[3] = new Replacement(3, 2, new string[] { "20" });
+            var changes1 = new Dictionary<int, IMergableChange>()
+            {
+                { 1, new Addition(1, new string[] { "10" }) },
+                { 2, new Removal(2, 1) },
+                { 3, new Replacement(3, 2, new string[] { "20" }) }
+            };
 
-            var changes2 = new Dictionary<int, IMergableChange>();
-            changes2[1] = new Addition(1, new string[] { "30" });
-            changes2[3] = new Replacement(3, 2, new string[] { "40" });
+            var changes2 = new Dictionary<int, IMergableChange>()
+            {
+                { 1, new Addition(1, new string[] { "30" }) },
+                { 3, new Replacement(3, 2, new string[] { "40" }) }
+            };
 
             IDictionary<int, IChange> result = merger.Merge(changes1, changes2, 5);
             MergersCommonTests.CheckDictionaries(changes1, result);
