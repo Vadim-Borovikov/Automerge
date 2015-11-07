1363a5ed707add18a667c547d2db0269fdc74edb
Vadim Borovikov <vadim.borovikov@gmail.com>
2015-11-07 03:38:01 +0300

    More accurate conflict strings

--- Automerger/Model/Consts.cs
+++ Automerger/Model/Consts.cs
@@ -2,10 +2,10 @@
 {
     public static class Consts
     {
-        public const string CONFLICT_BLOCK_BEGIN = ">>>>> BEGIN OF CONFLICTED BLOCK >>>>>\n";
-        public const string CONFLICT_BLOCK_SOURCE = ">>>>> ORIGINAL VERSION          >>>>>\n";
-        public const string CONFLICT_BLOCK_CHANGED1 = ">>>>> MODIFIED VERSION 1        >>>>>\n";
-        public const string CONFLICT_BLOCK_CHANGED2 = ">>>>> MODIFIED VERSION 2        >>>>>\n";
-        public const string CONFLICT_BLOCK_END = ">>>>> END OF CONFLICTED BLOCK   >>>>>\n";
+        public const string CONFLICT_BLOCK_BEGIN    = ">>>>> BEGIN OF CONFLICTED BLOCK >>>>>";
+        public const string CONFLICT_BLOCK_SOURCE   = ">>>>> ORIGINAL VERSION          >>>>>";
+        public const string CONFLICT_BLOCK_CHANGED1 = ">>>>> MODIFIED VERSION 1        >>>>>";
+        public const string CONFLICT_BLOCK_CHANGED2 = ">>>>> MODIFIED VERSION 2        >>>>>";
+        public const string CONFLICT_BLOCK_END      = ">>>>> END OF CONFLICTED BLOCK   >>>>>";
     }
 }
